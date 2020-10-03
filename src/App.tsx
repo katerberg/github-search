9c734b7f0ac4b0e19daab3236107661b02d813b9
Mark Katerberg <katerberg@fastmail.fm>
2020-10-03 10:42:44 -0500

    Pulls in material so I don't have to spend all morning writing CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
+import Button from '@material-ui/core/Button';
 import './App.css';
 
 export function App(): JSX.Element {
@@ -7,6 +8,7 @@ export function App(): JSX.Element {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Button>hello</Button>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
