e9ea4e25b015ece488579a811b6703b31ca91bbf
Mark Katerberg <katerberg@fastmail.fm>
2020-10-03 16:53:05 -0500

    Updates

---
+++ precache-manifest.00df4222e37923c71dd9bf35d2140f90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96bb80fd9364af956dc7d3c716d93709",
+    "url": "/github-search/index.html"
+  },
+  {
+    "revision": "e54eb0fa0acdef678db7",
+    "url": "/github-search/static/css/main.16b55e95.chunk.css"
+  },
+  {
+    "revision": "253c9f64e41642e07ec2",
+    "url": "/github-search/static/js/2.dd0957ff.chunk.js"
+  },
+  {
+    "revision": "0c7f7415c0ec92798fd914561f3edc54",
+    "url": "/github-search/static/js/2.dd0957ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e54eb0fa0acdef678db7",
+    "url": "/github-search/static/js/main.d3043be2.chunk.js"
+  },
+  {
+    "revision": "0184107a1cd9cd300d28",
+    "url": "/github-search/static/js/runtime-main.0993e2ff.js"
+  }
+]);
