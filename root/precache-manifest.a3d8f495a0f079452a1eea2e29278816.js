b0e363e122b325c73acce06b70287fac2b01951a
Mark Katerberg <katerberg@fastmail.fm>
2020-10-03 10:13:31 -0500

    Build artifacts for github pages

---
+++ root/precache-manifest.a3d8f495a0f079452a1eea2e29278816.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac3cc6c64020eb1a8dad520e380d0e7c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a7da13795780543b3de",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cd5d4a250d4cd5a2563a",
+    "url": "/static/js/2.07d36ee5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.07d36ee5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a7da13795780543b3de",
+    "url": "/static/js/main.bfd54815.chunk.js"
+  },
+  {
+    "revision": "864964386b8fbd0a16b5",
+    "url": "/static/js/runtime-main.0fe5df00.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
