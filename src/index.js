d7be38ce667b0cb7c7e6f592886003788c296c65
Mark Katerberg <katerberg@fastmail.fm>
2020-10-03 16:49:32 -0500

    Clean up dead code

--- src/index.js
+++ src/index.js
@@ -2,12 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import {App} from './App';
-// import {User} from './User';
-// import * as fake from './mockResponse.json';
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <User data={fake[0]} /> */}
     <App />
   </React.StrictMode>,
   document.getElementById('root')
